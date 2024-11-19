7492213d2a4ec5d248a0aa9d87ba2bd44e44c5ae
Ali Qeblawi <aqeblawi@c4b10c4.42berlin.de>
2024-11-19 21:49:30 +0100

    Feat: all function prototypes are done so far

--- include/utils.h
+++ include/utils.h
@@ -17,25 +17,25 @@
 #define MAX_TIME 100
 
 // ANSI Color codes
-#define ANSI_COLOR_RED     "\033[0;31m"
+/*#define ANSI_COLOR_RED     "\033[0;31m"
 #define ANSI_COLOR_GREEN   "\033[0;32m"
 #define ANSI_COLOR_YELLOW  "\033[0;33m"
 #define ANSI_COLOR_BLUE    "\033[0;34m"
-#define ANSI_COLOR_RESET   "\033[0m"
+define ANSI_COLOR_RESET   "\033[0m"*/
 
 // Error messages
 #define ERR_MEMORY "Failed to allocate memory\n"
 #define ERR_INPUT "Invalid input\n"
 
 // Function declarations
-void print_job_header(void);
-void print_job_status(job* current_job, int elapsed_time, const char* status);
-void print_job_progress(job* current_job, int pages_processed);
-void print_job_completion(job* current_job);
+//void print_job_header(void);
+//void print_job_status(job* current_job, int elapsed_time, const char* status);
+//void print_job_progress(job* current_job, int pages_processed);
+//void print_job_completion(job* current_job);
 int find_next_job(job* jobs, int num_jobs, const char* type, int elapsed_time);
 // Function declarations for different job scheduling modes
-void schedule_jobs_unsync(job* jobs, int num_jobs);       // For unsynchronized scheduling
-void schedule_jobs_mutex(job* jobs, int num_jobs);        // For mutex-synchronized scheduling
-void schedule_jobs_semaphore(job* jobs, int num_jobs);    // For semaphore-synchronized scheduling
+//void schedule_jobs_unsync(job* jobs, int num_jobs);       // For unsynchronized scheduling
+void mutex(job* jobs, int num_jobs);       // For mutex-synchronized scheduling
+//void schedule_jobs_semaphore(job* jobs, int num_jobs);    // For semaphore-synchronized scheduling
 void schedule_jobs(job* jobs, int num_jobs); 
 #endif 
